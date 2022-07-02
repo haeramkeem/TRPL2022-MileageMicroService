aca0c1b917ef8bcca249fb3c05cf988601f34edf
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-03 02:11:51 +0900

    feat: update place service

--- src/places/places.module.ts
+++ src/places/places.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlacesService } from './places.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Place } from './entities/place.entity';
+import { Review } from 'src/reviews/entities/review.entity';
 
 @Module({
-    providers: [PlacesService]
+    imports:    [ TypeOrmModule.forFeature([ Place, Review ]) ],
+    providers:  [ PlacesService ],
+    exports:    [ PlacesService ]
 })
 export class PlacesModule {}
--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { UpdatePlaceDto } from './dto/update-place.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Review} from 'src/reviews/entities/review.entity';
+import {Repository} from 'typeorm';
+import {Place} from './entities/place.entity';
 
 @Injectable()
 export class PlacesService {
-    update(id: number, updatePlaceDto: UpdatePlaceDto) {
-        return `This action updates a #${id} place`;
+    constructor(
+        @InjectRepository(Place) private placeRepository: Repository<Place>,
+        @InjectRepository(Review) private reviewRepository: Repository<Review>,
+    ) {}
+
+    async update(placeId: string, firstReviewId: string) {
+        const firstReview = await this.reviewRepository.findOne({
+            where: { id: firstReviewId },
+        });
+
+        // TODO: NotFoundException for firstReview
+        // TODO: Update only when the firstReview is nil
+
+        this.placeRepository.update({ id: placeId }, { firstReview });
+        // TODO: NotFoundException for place
+        // TODO: return the number of affected rows
     }
 }
