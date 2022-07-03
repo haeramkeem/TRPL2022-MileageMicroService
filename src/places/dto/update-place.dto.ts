fa45df5e11d0d7a059df4878cc6a2c41372c31ff
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-03 11:12:22 +0900

    fix: validate update place & update only when first review is null

--- src/places/dto/update-place.dto.ts
+++ src/places/dto/update-place.dto.ts
@@ -1,8 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { IsUUID } from 'class-validator';
-import { CreatePlaceDto } from './create-place.dto';
 
-export class UpdatePlaceDto extends PartialType(CreatePlaceDto) {
+export class UpdatePlaceDto {
+    @IsUUID()
+    placeId: string;
+
     @IsUUID()
     firstReviewId: string;
 }
--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Review} from 'src/reviews/entities/review.entity';
 import {Repository} from 'typeorm';
+import {UpdatePlaceDto} from './dto/update-place.dto';
 import {Place} from './entities/place.entity';
 
 @Injectable()
@@ -11,16 +12,26 @@ export class PlacesService {
         @InjectRepository(Review) private reviewRepository: Repository<Review>,
     ) {}
 
-    async update(placeId: string, firstReviewId: string) {
-        const firstReview = await this.reviewRepository.findOne({
-            where: { id: firstReviewId },
-        });
+    async update(validated: UpdatePlaceDto) {
+        const id = validated.placeId;
+        const place = await this.placeRepository.findOne({ where: { id } });
+        if (!place) {
+            console.error("PlaceNotFound")
+            return; // TODO: NotFoundException for place
+        }
+        if (place.firstReview) {
+            console.log(place.firstReview);
+            return; // Abort: Update place only when firstReview is nil
+        }
 
-        // TODO: NotFoundException for firstReview
-        // TODO: Update only when the firstReview is nil
+        const firstReview = await this.reviewRepository.findOne({ where: { id: validated.firstReviewId } });
+        if (!firstReview) {
+            console.error("ReviewNotFound")
+            return; // TODO: NotFoundException for firstReview
+        }
 
-        this.placeRepository.update({ id: placeId }, { firstReview });
-        // TODO: NotFoundException for place
-        // TODO: return the number of affected rows
+        await this.placeRepository.update({ id }, { firstReview });
+
+        // TODO: return the number of affected rows or subscriber
     }
 }
