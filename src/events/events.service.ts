ce6474780854828e7d05dcd8279e7b48809dd859
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-05 00:37:47 +0900

    feat: update review prototype

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -19,7 +19,7 @@ export class EventsService {
         review.content = dto.content;
         review.author = await queryRunner.manager.findOneBy(User, { id: dto.userId }); // TODO: user not found
         review.place = await queryRunner.manager.findOneBy(Place, { id: dto.placeId }); // TODO: place not found
-        await queryRunner.startTransaction()
+        await queryRunner.startTransaction();
 
         try {
             // Insert review
@@ -31,7 +31,7 @@ export class EventsService {
             await queryRunner.manager.save(dto.attachedPhotoIds.map(photoId => {
                 const photo = new Photo();
                 photo.id = photoId;
-                // photo.attachedReview = review;
+                photo.attachedReview = review;
                 return photo;
             }));
 
@@ -49,14 +49,48 @@ export class EventsService {
         } catch (err) {
             // Rollback for DB fault
             await queryRunner.rollbackTransaction();
+            // TODO: rollback subscribed action
         } finally {
             // Defer
             await queryRunner.release();
         }
     }
 
-    update(id: string, updateEventDto: UpdateEventDto) {
-        return `This action updates a #${id} event`;
+    async update(id: string, dto: UpdateEventDto) {
+        const queryRunner = this.dataSource.createQueryRunner(); // TODO: QueryRunnerFactory
+        await queryRunner.connect();
+
+        const review = await queryRunner.manager.findOneBy(Review, { id });
+        await queryRunner.startTransaction();
+
+        try {
+            // Update content
+            // TODO: subscribe to review content update
+            await queryRunner.manager.update(Review, { id }, { content: dto.content });
+
+            // Soft delete photos
+            // TODO: subscribe to delete photo event
+            await queryRunner.manager.softDelete(Photo, { attachedReview: review });
+
+            // Insert photos
+            // TODO: subscribe to save photo event
+            await queryRunner.manager.save(dto.attachedPhotoIds.map(photoId => {
+                const photo = new Photo();
+                photo.id = photoId;
+                photo.attachedReview = review;
+                return photo;
+            }));
+
+            // Commit transaction
+            await queryRunner.commitTransaction();
+        } catch (err) {
+            // Rollback for DB fault
+            await queryRunner.rollbackTransaction();
+            // TODO: rollback subscribed action
+        } finally {
+            // Defer
+            await queryRunner.release();
+        }
     }
 
     remove(id: string) {
